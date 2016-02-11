3ee8b195c9802147cb065c7f137b49b653cbaf6a
Mikeman3561 <Mikeman3561@gmail.com>
2016-02-10 22:28:23 -0500

    ChessPiece.java

--- ChessGameFrontEnd/src/ChessPiece.java
+++ ChessGameFrontEnd/src/ChessPiece.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
 
 public interface ChessPiece {
 
+	public int x = 0;
+	public int y = 0;
+	public String color = "White";
+	public String type = "King";
+	public boolean isActive = true;
+	
+	public ArrayList<ArrayList<Integer>> getMoves();
 }
