dae87a1852d3a0bf0a33f4778dfc2024c53f058b
Mikeman3561 <Mikeman3561@gmail.com>
2016-02-26 01:20:51 -0500

    Board Update
    Board Update

--- ChessGameFrontEnd/src/Board.java
+++ ChessGameFrontEnd/src/Board.java
@@ -1,75 +1,73 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-
 import java.lang.Object;
 
 public class Board {
 
-	public Board instance;
-	
-	private ChessGUI gui;
-	
+	public static Board instance;
+
+	private static ChessGUI gui;
+
 	private Piece[][] boardState;
-	
+
 	public List<Piece> blackPieces;
 	public List<Piece> whitePieces;
-	
-	public int turn = 0;	
-	
-	public Board(){
+
+	public int turn = 0;
+
+	public Board() {
 		ChessGUI cg = new ChessGUI();
 
-        JFrame f = new JFrame("Chess Game");
-        f.add(cg.getGui());
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        f.setLocationByPlatform(true);
-        f.pack();
-        f.setMinimumSize(f.getSize());
-        f.setVisible(true);
+		JFrame f = new JFrame("Chess Game");
+		f.add(cg.getGui());
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		f.setLocationByPlatform(true);
+		f.pack();
+		f.setMinimumSize(f.getSize());
+		f.setVisible(true);
 	}
-	
+
 	public void updateBoard() {
-		
+
 	}
-	
+
 	public boolean isChecked(int player) {
 		return false;
 	}
-	
+
 	public boolean isCheckMate(int player) {
 		return false;
 	}
-	
-	public ArrayList<ArrayList<Integer>> showLegalMoves(ArrayList<ArrayList<Integer>> spaces) {
-		return spaces;	
+
+	public ArrayList<ArrayList<Integer>> getMoves(ArrayList<ArrayList<Integer>> spaces) {
+		return spaces;
 	}
 
-	public Piece[][] getState(){
+	public Piece[][] getState() {
 		return boardState;
 	}
-	
-	public void incrementTurn(){
+
+	public void incrementTurn() {
 		turn++;
 	}
-	
-	public void makeMove() {
-		
-	}
-	
-	 public static void main(String[] args) {
-	        Runnable r = new Runnable() {
 
-	            @Override
-	            public void run() {
-	            	Board board = new Board();
-	            }
-	        };
-	        // Swing GUIs should be created and updated on the EDT
-	        // http://docs.oracle.com/javase/tutorial/uiswing/concurrency
-	        SwingUtilities.invokeLater(r);
-	 }
+	public void makeMove() {
+
+	}
+
+	public static void main(String[] args) {
+		Runnable r = new Runnable() {
+
+			@Override
+			public void run() {
+				Board board = new Board();
+			}
+		};
+		// Swing GUIs should be created and updated on the EDT
+		// http://docs.oracle.com/javase/tutorial/uiswing/concurrency
+		SwingUtilities.invokeLater(r);
+	}
 }
