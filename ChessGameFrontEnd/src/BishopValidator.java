e93695a3fce94ebf99f82d432a9c9712b2547dc0
John Gall <Tigerquan@aol.com>
2016-03-05 19:52:29 -0500

    Bishop joins the fray!
    Bishop now knows how to stop.

--- ChessGameFrontEnd/src/BishopValidator.java
+++ ChessGameFrontEnd/src/BishopValidator.java
@@ -20,19 +20,179 @@ public class BishopValidator extends Validator  {
 	@Override
 	public ArrayList<int []> highlightBoard(int x, int y, boolean isWhite) {
 		ArrayList<int []> returnMoves = new ArrayList<int []>();
-		
-		for(int i=0; i <= 7; i++)
-		{
-			for(int j=0; j <= 7; j++)
-			{
-				int diffRow = Math.abs(i-x);
-				int diffCol = Math.abs(j-y);
-				
-				if(diffRow == diffCol)
-					returnMoves.add(new int [] {i, j});
+		if(isWhite) {
+			// UpLeft
+			for(int i=x-1; i>=0; i--) {
+				for(int j=y-1; j>=0; j--) {
+					int diffRow = Math.abs(i-x);
+					int diffCol = Math.abs(j-y);
+					if(diffRow == diffCol) {
+						if (Board.boardState[i][j] == null) {
+							returnMoves.add(new int [] {i, j});
+						} else {
+							if(Board.boardState[i][j].getIsWhite() == false) {
+								returnMoves.add(new int [] {i, j});
+								break;
+							} else {
+								if (Board.boardState[i][j].getIsWhite() == true) {
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			// UpRight
+			for(int i=x+1; i<=7; i++) {
+				for(int j=y-1; j>=0; j--) {
+					int diffRow = Math.abs(i-x);
+					int diffCol = Math.abs(j-y);
+					if(diffRow == diffCol) {
+						if (Board.boardState[i][j] == null) {
+							returnMoves.add(new int [] {i, j});
+						} else {
+							if(Board.boardState[i][j].getIsWhite() == false) {
+								returnMoves.add(new int [] {i, j});
+								break;
+							} else {
+								if (Board.boardState[i][j].getIsWhite() == true) {
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			// DownRight
+			for(int i=x+1; i<=7; i++) {
+				for(int j=y+1; j<=7; j++) {
+					int diffRow = Math.abs(i-x);
+					int diffCol = Math.abs(j-y);
+					if(diffRow == diffCol) {
+						if (Board.boardState[i][j] == null) {
+							returnMoves.add(new int [] {i, j});
+						} else {
+							if(Board.boardState[i][j].getIsWhite() == false) {
+								returnMoves.add(new int [] {i, j});
+								break;
+							} else {
+								if (Board.boardState[i][j].getIsWhite() == true) {
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			// DownLeft
+			for(int i=x-1; i>=0; i--) {
+				for(int j=y+1; j<=7; j++) {
+					int diffRow = Math.abs(i-x);
+					int diffCol = Math.abs(j-y);
+					if(diffRow == diffCol) {
+						if (Board.boardState[i][j] == null) {
+							returnMoves.add(new int [] {i, j});
+						} else {
+							if(Board.boardState[i][j].getIsWhite() == false) {
+								returnMoves.add(new int [] {i, j});
+								break;
+							} else {
+								if (Board.boardState[i][j].getIsWhite() == true) {
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+		} else {
+			if(!isWhite) {
+				// UpLeft
+				for(int i=x-1; i>=0; i--) {
+					for(int j=y-1; j>=0; j--) {
+						int diffRow = Math.abs(i-x);
+						int diffCol = Math.abs(j-y);
+						if(diffRow == diffCol) {
+							if (Board.boardState[i][j] == null) {
+								returnMoves.add(new int [] {i, j});
+							} else {
+								if(Board.boardState[i][j].getIsWhite() == true) {
+									returnMoves.add(new int [] {i, j});
+									break;
+								} else {
+									if (Board.boardState[i][j].getIsWhite() == false) {
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+				// UpRight
+				for(int i=x+1; i<=7; i++) {
+					for(int j=y-1; j>=0; j--) {
+						int diffRow = Math.abs(i-x);
+						int diffCol = Math.abs(j-y);
+						if(diffRow == diffCol) {
+							if (Board.boardState[i][j] == null) {
+								returnMoves.add(new int [] {i, j});
+							} else {
+								if(Board.boardState[i][j].getIsWhite() == true) {
+									returnMoves.add(new int [] {i, j});
+									break;
+								} else {
+									if (Board.boardState[i][j].getIsWhite() == false) {
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+				// DownRight
+				for(int i=x+1; i<=7; i++) {
+					for(int j=y+1; j<=7; j++) {
+						int diffRow = Math.abs(i-x);
+						int diffCol = Math.abs(j-y);
+						if(diffRow == diffCol) {
+							if (Board.boardState[i][j] == null) {
+								returnMoves.add(new int [] {i, j});
+							} else {
+								if(Board.boardState[i][j].getIsWhite() == true) {
+									returnMoves.add(new int [] {i, j});
+									break;
+								} else {
+									if (Board.boardState[i][j].getIsWhite() == false) {
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+				// DownLeft
+				for(int i=x-1; i>=0; i--) {
+					for(int j=y+1; j<=7; j++) {
+						int diffRow = Math.abs(i-x);
+						int diffCol = Math.abs(j-y);
+						if(diffRow == diffCol) {
+							if (Board.boardState[i][j] == null) {
+								returnMoves.add(new int [] {i, j});
+							} else {
+								if(Board.boardState[i][j].getIsWhite() == true) {
+									returnMoves.add(new int [] {i, j});
+									break;
+								} else {
+									if (Board.boardState[i][j].getIsWhite() == false) {
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
-		
 		return returnMoves;
 	}
 }
