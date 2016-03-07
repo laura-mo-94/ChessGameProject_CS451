435b04887ce2fd5fcb1451ddc7fdd5187ef88251
Aurelis-Loire <johanna.oberto@gmail.com>
2016-03-07 00:06:39 -0500

    You're waiting for a train
    A train that'll take you far away. You know where you hope this train
    will take you. But you can't know for sure. Yet it doesn't matter. Now,
    tell me why?
    BECAUSE THIS PROJECT WILL BE OVER!

--- ChessGameFrontEnd/src/KingValidator.java
+++ ChessGameFrontEnd/src/KingValidator.java
@@ -64,7 +64,133 @@ public class KingValidator extends Validator  {
 				
 			}
 		}
-		returnMoves = tempArr;
+		/*Iterate through entire board state
+		check each space if it is the opponent’s color
+		if it is opponent’s color, check oppPiece’s validation (call highlight board)
+		if oppPiece validator array contains a space in the King’s possible movepool, 
+			delete that space from the King’s movepool
+
+		saves places king can’t move to
+		afterward, iterate through king’s possible moves and discard any commonalities with the can’t moves.
+		*/	
+		ArrayList<int []> interferingMoves = new ArrayList<int []>();
+		
+		if(isWhite)
+		{
+			//iterates through rows
+			for(int i=0; i<8; i++)
+			{
+				//iterates through columns
+				for(int j=0; j < 8; j++)
+				{
+					//check if is opponent piece
+					if(Board.boardState[i][j] != null)
+					{
+						if(Board.boardState[i][j].getIsWhite() == false)
+						{
+							//AVOID INFINITE LOOPS just s'cause
+							if(Board.boardState[i][j].getType().toString() == "KING")
+							{
+								System.out.println("LOL");
+							}
+							else {
+								//save array of opponent possible moves
+								ArrayList<int []> oppMoves = new ArrayList<int []>();
+								oppMoves = Board.boardState[i][j].getValidator().highlightBoard(i, j, false);
+								//run through king array
+								for(int k=0; k < tempArr.size(); k++)
+								{
+									//check for duplicates in opponent moves
+									for(int l=0; l < oppMoves.size(); l++)
+									{
+										//check x coordinates
+										if(tempArr.get(k)[0] == oppMoves.get(l)[0])
+										{
+											//check y coordinates
+											if(tempArr.get(k)[1] == oppMoves.get(l)[1])
+											{
+												System.out.println("\n" + "\n" + "--------------------------" + "\n" + "THIS SHIT IS HERE");
+												System.out.println(oppMoves.get(l));
+												interferingMoves.add(oppMoves.get(l));
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		if(!isWhite)
+		{
+			//iterates through rows
+			for(int i=0; i<8; i++)
+			{
+				//iterates through columns
+				for(int j=0; j < 8; j++)
+				{
+					//check if is opponent piece
+					if(Board.boardState[i][j] != null)
+					{
+						if(Board.boardState[i][j].getIsWhite() == true)
+						{
+							//AVOID INFINITE LOOPS just s'cause
+							if(Board.boardState[i][j].getType().toString() == "KING")
+							{
+								System.out.println("LOL");
+							}
+							else {
+								//save array of opponent possible moves
+								ArrayList<int []> oppMoves = new ArrayList<int []>();
+								oppMoves = Board.boardState[i][j].getValidator().highlightBoard(i, j, true);
+								//run through king array
+								for(int k=0; k < tempArr.size(); k++)
+								{
+									//check for duplicates in opponent moves
+									for(int l=0; l < oppMoves.size(); l++)
+									{
+										//check x coordinates
+										if(tempArr.get(k)[0] == oppMoves.get(l)[0])
+										{
+											//check y coordinates
+											if(tempArr.get(k)[1] == oppMoves.get(l)[1])
+											{
+												System.out.println("\n" + "\n" + "--------------------------" + "\n" + "THIS SHIT IS HERE");
+												System.out.println(oppMoves.get(l));
+												interferingMoves.add(oppMoves.get(l));
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		
+		ArrayList<int []> temperTempArr = new ArrayList<int []>();
+		
+		for(int i=0; i<tempArr.size(); i++)
+		{
+			for(int j=0; j<interferingMoves.size(); j++)
+			{
+				if((tempArr.get(i)[0] == interferingMoves.get(j)[0]) && (tempArr.get(i)[1] == interferingMoves.get(j)[1]))
+				{
+					
+				}
+				else
+				{
+					temperTempArr.add(tempArr.get(i));
+				}
+						
+			}
+		}
+		
+		returnMoves = temperTempArr;
 		
 		return returnMoves;
 	}
